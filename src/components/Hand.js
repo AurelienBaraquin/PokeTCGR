369d5dcbecc0725ea125f9e9b4a32463af2b3ee2
AurelienBaraquin <aurelien.baraquin@epitech.eu>
2025-03-06 20:57:21 +0100

    Enhance card selection logic and update display in Hand component

--- src/components/Hand.js
+++ src/components/Hand.js
@@ -2,21 +2,32 @@ import { Card, CardWidth, CardHeight } from './Card.js';
 import { CardPile } from './CardPile.js';
 
 export class Hand extends CardPile {
+    constructor(scene, x, y, scale = 1) {
+        super(scene, x, y, scale);
+        this.selectedCard = null;
+    }
+
     updateDisplay() {
-        const spacingX = CardWidth() * this.scale * 0.3;
+        const spacingX = CardWidth() * this.scale * 0.35;
         const startX = this.x - ((this.cards.length - 1) * spacingX) / 2;
 
         this.cards.forEach((card, index) => {
             card.container.x = startX + index * spacingX;
-            card.container.y = this.y;
+            card.container.y = (card === this.selectedCard) ? this.y - 20 : this.y;
             card.container.setDepth(index);
         });
+        this.updateInteractivity();
     }
 
     updateInteractivity() {
         this.cards.forEach(card => {
             card.updateInteractive(() => {
-                this.removeCard(card);
+                if (this.selectedCard === card) {
+                    this.selectedCard = null;
+                } else {
+                    this.selectedCard = card;
+                }
+                this.updateDisplay();
             });
         });
     }
